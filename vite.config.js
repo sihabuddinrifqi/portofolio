3712647ae5b08212dacda131918c0afe418d63ca
sihabuddinrifqi <syihabuddinrifqi@gmail.com>
2025-01-11 10:25:48 +0700

    ke dua

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000, // Port lokal (opsional)
-  },
   build: {
-    outDir: 'dist', // Folder output
-    chunkSizeWarningLimit: 1000,
+    outDir: 'dist', // Output folder
+    chunkSizeWarningLimit: 1000, // Optional: Atur batas ukuran chunk
   },
 });
-
